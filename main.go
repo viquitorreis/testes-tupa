d14a0a56177f99b78b6bee4d997942580abd8c3c
victor <victorreisprog@gmail.com>
2024-01-29 11:47:43 -0300

    fix: arrumando ordem codigo

--- main.go
+++ main.go
@@ -63,52 +63,6 @@ func FmtYellow(s string) string {
 	return string("\033[1;33m ") + s + string("\033[0m")
 }
 
-func WriteJSONHelper(w http.ResponseWriter, status int, v any) error {
-	w.Header().Set("Content-Type", "application/json")
-
-	if w == nil {
-		return errors.New("response writer passado está nulo")
-	}
-
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(v)
-}
-
-func (c *Controller) MakeHTTPHandlerFuncHelper(f APIFunc, httpMethod HTTPMethod) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		if r.Method == string(httpMethod) {
-			if err := f(ctx, w, r); err != nil {
-				if err := WriteJSONHelper(w, http.StatusInternalServerError, APIError{Error: err.Error()}); err != nil {
-					fmt.Println("Erro ao escrever resposta JSON:", err)
-				}
-			}
-		} else {
-			WriteJSONHelper(w, http.StatusMethodNotAllowed, APIError{Error: "Método HTTP não permitido"})
-		}
-	}
-}
-
-func (c *Controller) RegisterRoutes(router *mux.Router, route string, handlers map[HTTPMethod]APIFunc) {
-	for method, handler := range handlers {
-		if !AllowedMethods[method] {
-			log.Fatal(fmt.Sprintf(FmtRed("Método HTTP não permitido: "), "%s\nVeja como criar um novo método na documentação", method))
-		}
-		router.HandleFunc(route, c.MakeHTTPHandlerFuncHelper(handler, method)).Methods(string(method))
-	}
-}
-
-func (a *APIServer) SetDefaultRoute(handlers map[HTTPMethod]APIFunc) {
-	defController := Controller{}
-	for method, handler := range handlers {
-		a.router.HandleFunc("/", defController.MakeHTTPHandlerFuncHelper(handler, method)).Methods(string(method))
-	}
-}
-
-func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
-	WriteJSONHelper(w, http.StatusOK, "Seja bem vindo ao Tupã framework!")
-}
-
 func (a *APIServer) New() {
 
 	if a.router.GetRoute("/") == nil {
@@ -142,7 +96,6 @@ func (a *APIServer) New() {
 		log.Fatal(FmtRed("Erro ao desligar servidor: "), err)
 	}
 
-	///////////////// NAO TA CHEGANDO AQ
 	fmt.Println(FmtYellow("Servidor encerrado na porta: " + a.listenAddr))
 
 }
@@ -152,6 +105,52 @@ func NewApiServer(listenAddr string) *APIServer {
 		listenAddr: listenAddr,
 		router:     mux.NewRouter(),
 		// store:      store,
+	}
+}
+
+func (a *APIServer) SetDefaultRoute(handlers map[HTTPMethod]APIFunc) {
+	defController := Controller{}
+	for method, handler := range handlers {
+		a.router.HandleFunc("/", defController.MakeHTTPHandlerFuncHelper(handler, method)).Methods(string(method))
+	}
+}
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	WriteJSONHelper(w, http.StatusOK, "Seja bem vindo ao Tupã framework!")
+}
+
+func (c *Controller) RegisterRoutes(router *mux.Router, route string, handlers map[HTTPMethod]APIFunc) {
+	for method, handler := range handlers {
+		if !AllowedMethods[method] {
+			log.Fatal(fmt.Sprintf(FmtRed("Método HTTP não permitido: "), "%s\nVeja como criar um novo método na documentação", method))
+		}
+		router.HandleFunc(route, c.MakeHTTPHandlerFuncHelper(handler, method)).Methods(string(method))
+	}
+}
+
+func WriteJSONHelper(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	if w == nil {
+		return errors.New("response writer passado está nulo")
+	}
+
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func (c *Controller) MakeHTTPHandlerFuncHelper(f APIFunc, httpMethod HTTPMethod) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		if r.Method == string(httpMethod) {
+			if err := f(ctx, w, r); err != nil {
+				if err := WriteJSONHelper(w, http.StatusInternalServerError, APIError{Error: err.Error()}); err != nil {
+					fmt.Println("Erro ao escrever resposta JSON:", err)
+				}
+			}
+		} else {
+			WriteJSONHelper(w, http.StatusMethodNotAllowed, APIError{Error: "Método HTTP não permitido"})
+		}
 	}
 }
 
